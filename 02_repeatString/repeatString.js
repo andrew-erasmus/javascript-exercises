10c4b9a4bad1a55a907d45c8f75e991226d63d91
andrew-erasmus <andrew.r.eras@gmail.com>
2023-06-20 16:52:30 +0200

    Complete exercises 1 to 7

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let output="";
 
+    for(let i = 0; i<num; i++){
+        output+=word;
+    }
+    return output;
 };
 
+repeatString("hey",3);
 // Do not edit below this line
 module.exports = repeatString;
